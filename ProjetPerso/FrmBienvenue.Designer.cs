423d4239d64e03ddf3d07deba6885dc1c213eb94
Noodra <nicolas.br@eduge.com>
2023-11-23 13:35:57 +0100

    Add projet on github

---
+++ ProjetPerso/FrmBienvenue.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace ProjetPerso
+{
+    partial class FrmBienvenue
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblBienvenue = new System.Windows.Forms.Label();
+            this.btnCommencer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblBienvenue
+            // 
+            this.lblBienvenue.AutoSize = true;
+            this.lblBienvenue.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBienvenue.Location = new System.Drawing.Point(225, 66);
+            this.lblBienvenue.Name = "lblBienvenue";
+            this.lblBienvenue.Size = new System.Drawing.Size(625, 42);
+            this.lblBienvenue.TabIndex = 0;
+            this.lblBienvenue.Text = "Bienvenue dans Leaderboard Maker";
+            // 
+            // btnCommencer
+            // 
+            this.btnCommencer.BackColor = System.Drawing.Color.LawnGreen;
+            this.btnCommencer.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCommencer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCommencer.Location = new System.Drawing.Point(435, 194);
+            this.btnCommencer.Name = "btnCommencer";
+            this.btnCommencer.Size = new System.Drawing.Size(164, 160);
+            this.btnCommencer.TabIndex = 1;
+            this.btnCommencer.Text = "Commencer";
+            this.btnCommencer.UseVisualStyleBackColor = false;
+            // 
+            // FrmBienvenue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.ForestGreen;
+            this.ClientSize = new System.Drawing.Size(1077, 564);
+            this.Controls.Add(this.btnCommencer);
+            this.Controls.Add(this.lblBienvenue);
+            this.Name = "FrmBienvenue";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Leaderboard Maker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBienvenue;
+        private System.Windows.Forms.Button btnCommencer;
+    }
+}
+
