b0a5fa6c2716742650c6115dcd0f68a2c846eab8
Blazer54 <pmf.marques.gva@gmail.com>
2023-11-23 16:02:54 +0100

    ajout fonctionalité bouton form Bienvenue

--- ProjetPerso/Frminfojoueurs.Designer.cs
+++ ProjetPerso/Frminfojoueurs.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ProjetPerso
 {
-    partial class Form1
+    partial class FrmInfoJoueurs
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmInfoJoueurs));
+            this.SuspendLayout();
+            // 
+            // FrmInfoJoueurs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(1077, 564);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmInfoJoueurs";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Information sur les joueurs";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
