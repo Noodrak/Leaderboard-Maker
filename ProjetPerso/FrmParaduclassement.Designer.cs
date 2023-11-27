f01a48fcf32c8e8cd3d196f6bbe299368d59d693
Noodra <nicolas.br@eduge.com>
2023-11-27 11:25:46 +0100

    ajout tbx

--- ProjetPerso/FrmParaduclassement.Designer.cs
+++ ProjetPerso/FrmParaduclassement.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.LblNbrjoueur = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // LblNbrjoueur
@@ -56,12 +57,20 @@
             this.comboBox1.Size = new System.Drawing.Size(121, 21);
             this.comboBox1.TabIndex = 1;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(510, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
             // FrmParaduclassement
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.LblNbrjoueur);
             this.Name = "FrmParaduclassement";
@@ -75,5 +84,6 @@
 
         private System.Windows.Forms.Label LblNbrjoueur;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
